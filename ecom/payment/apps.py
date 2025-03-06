e6116b7d521b65c5877450808fa2797c5ec051c2
Kparobookus <kparobookus@gmail.com>
2025-03-06 04:47:56 -1100

    Delete ecom/payment directory

--- ecom/payment/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class MyAppConfig(AppConfig):
-    name = 'payment'
-
-    def ready(self):
-        # Do not access models or the database here directly
-        pass
